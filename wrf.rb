e30af15e7f6366453ecff9ab0d7b791853317ce4
mcflugen <mcflugen@gmail.com>
2015-02-13 15:12:28 -0700

    Added formula for WRF.

---
+++ wrf.rb
@@ -0,0 +1,27 @@
+require "formula"
+require "open3"
+
+class Wrf < Formula
+  homepage "https://github.com/yyr/wrf"
+  head "https://github.com/yyr/wrf.git"
+  sha1 ""
+
+  depends_on :fortran
+  depends_on "homebrew/science/netcdf" => "enable-fortran" unless OS.linux?
+  depends_on "csdms/dupes/netcdf" => "enable-fortran" if OS.linux?
+
+  def install
+    ENV['NETCDF'] = Formula['netcdf'].prefix
+
+    Open3.popen3("./configure") do |stdin, stdout, _|
+      stdin.write "15\n"
+      stdin.write "0\n"
+      !!(stdout.read)
+    end
+    system "./compile em_real"
+  end
+
+  test do
+    system "false"
+  end
+end
