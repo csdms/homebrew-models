9972f9cfc87ac10ccc495ba0211fbabf65634040
Mark Piper <mark.piper@colorado.edu>
2015-03-06 15:02:37 -0700

    Update marssim URLs to point to csdms-contrib

--- marssim.rb
+++ marssim.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Marssim < Formula
   homepage "http://csdms.colorado.edu/wiki/Model:MARSSIM"
-  url "https://github.com/csdms/marssim.git", :tag => "3.0.0"
-  head "https://github.com/csdms/marssim.git"
+  url "https://github.com/csdms-contrib/marssim.git", :tag => "3.0.0"
+  head "https://github.com/csdms-contrib/marssim.git"
 
   depends_on "cmake" => :build
   depends_on :fortran
